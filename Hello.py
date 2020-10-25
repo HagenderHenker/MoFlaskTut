f69c846bc61676a77309eb6c86886cfe87b31a40
HagenderHenker <md1234567890@web.de>
2020-10-25 17:43:14 +0100

    Folge 3: variable Routen

--- Hello.py
+++ Hello.py
@@ -9,10 +9,24 @@ app = Flask(__name__)
 def index():
     return "Welcome to Flask"
 
-@app.route("/hello")   #Hier wird eine Unterseite als Route eingetragen
-def hello():
-    return "Hello World!"
+@app.route("/hello/<string:name>")   
 
+#auf der Unterseite wird ein Name als Parameter eingetragen. Der Name wird in der URL-Leiste auch benötigt um die Seite auszugeben
+#   mit dem Statement "STRING" wird der Variablen ein Datentyp zugewiesen """
+
+def hello(name):
+    return "Hello " + name + "!"
+
+
+@app.route("/addiere/<int:zahl>") # Versuch mit einer Zahl zur Berechnung, es geht auch z.B. float, path, 
+def addiere(zahl):
+    return str(zahl+zahl)  # ACHTUNG!! Ausgabe kann nur ein string, dict, tuple, Response instance, or WSGI callable sein 
+
+
+
+@app.route("/add2/<int:z1>/<int:z2>") # so gehen auch mehrere Parameter
+def add2(z1, z2):
+    return str(z1+z2)
 
 
 if __name__ == "__main__":
