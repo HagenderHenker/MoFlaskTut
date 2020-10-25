86877829ff041f78e810da278bc56691436d3d64
HagenderHenker <md1234567890@web.de>
2020-10-25 13:22:43 +0100

    Folge 2 eine zweite Seite eintragen und routen

--- Hello.py
+++ Hello.py
@@ -4,11 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/")   # Hier wird die Indexseite (also Homepage) also Route eingetragen
 
 def index():
+    return "Welcome to Flask"
+
+@app.route("/hello")   #Hier wird eine Unterseite als Route eingetragen
+def hello():
     return "Hello World!"
+
+
 
 if __name__ == "__main__":
     app.run(port=1337, debug=True)
-    
